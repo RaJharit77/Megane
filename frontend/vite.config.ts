ca987ba3606395e301f4a6318b07348ed059a041
RaJharit77 <hei.rajo.2@gmail.com>
2026-02-12 14:41:05 +0300

    feat: Update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'https://megane-gen-api.vercel.app',
         changeOrigin: true,
       }
     }
