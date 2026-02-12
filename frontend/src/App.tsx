270810064ed657f270daa56b12a234c13cb576e4
RaJharit77 <hei.rajo.2@gmail.com>
2026-02-12 08:38:36 +0300

    feat: Update App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import FormSection from './components/FormSection'
 import ResultSection from './components/ResultSection'
 import Confetti from 'react-confetti'
 import { Toaster } from 'react-hot-toast'
+import { Analytics } from '@vercel/analytics/react';
 
 type Flower = {
   type: string
@@ -142,6 +143,7 @@ function App() {
           },
         }}
       />
+      <Analytics />
     </div>
   )
 }
