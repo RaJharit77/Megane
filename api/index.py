8689bbeb5acef71a434ced6973f3285c0e2e1e30
RaJharit77 <hei.rajo.2@gmail.com>
2026-02-12 11:48:41 +0300

    feat: Update index files

--- api/index.py
+++ api/index.py
@@ -8,8 +8,12 @@ import io
 from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageEnhance, ImageChops
 import base64
 import numpy as np
+from mangum import Mangum # type: ignore
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
+
+handler = Mangum(app)
+
 CORS(app)
 
 # Messages d'amour améliorés
